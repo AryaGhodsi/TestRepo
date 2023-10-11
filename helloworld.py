ee618b125512c83ebe21ee4650289830cf163ced
Arya <3314805+aghodsi@users.noreply.github.com>
2023-10-11 17:24:14 +0200

    Create helloworld.py

---
+++ helloworld.py
@@ -0,0 +1,2 @@
+password="https://storbms.blob.core.windows.net/$logs?sp=r&st=2023-10-11T12:51:26Z&se=2023-10-11T20:51:26Z&spr=https&sv=2022-11-02&sr=c&sig=Q4UrntDEr2WPPKhb6puBLBv4e0jvWTx62hnxuk7UDJM%3D
+sp=r&st=2023-10-11T12:51:26Z&se=2023-10-11T20:51:26Z&spr=https&sv=2022-11-02&sr=c&sig=Q4UrntDEr2WPPKhb6puBLBv4e0jvWTx62hnxuk7UDJM%3D"
